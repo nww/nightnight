26e875f89fe0207e46eacbeae8fd9d6632f48394
Maksim V <maks112v@gmail.com>
2019-03-01 16:28:33 -0500

    Added Cookie to keep disabled if button clicked
    The night mode will be disabled for the user for 7 days after selected.

--- appengine/codeserver/nightnight.js
+++ appengine/codeserver/nightnight.js
@@ -1,18 +1,42 @@
-;
+/** nightnight.みんな v1.0 | MIT License | https://git.io/vwz9N */
+
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  var expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
 
 window.nightnightHide = function() {
 	var banners = document.getElementsByClassName('nightnight');
 
 	for (var i = 0; i < banners.length; i++) {
 		banners[i].style.display = 'none';
-	}
+  }
+  setCookie("nightnightOff", true, 7);
 };
 
 (function(){
+  console.log()
 	var d = new Date();
 	var m = d.getHours() * 60 + d.getMinutes();
-	var bh = {{ .BeginHours }}, bm = {{ .BeginMinutes }};
-	var eh = {{ .EndHours }}, em = {{ .EndMinutes }};
+	var bh = 22, bm = 0;
+	var eh = 6, em = 0;
 
 	var use12HourClock = true;
 
@@ -25,34 +49,35 @@ window.nightnightHide = function() {
 	}
 
 	if (m >= b || m <= e) {
-		console.log('NightNight activated')
+    if(!getCookie('nightnightOff')){
 
-		// Inject CSS
-		var css = '.nightnight {background: #000; font-family: Arial, sans-serif; color: #fff; font-size: 12px; padding: 10%; position: fixed; margin: 0; top: 0; left: 0; width: 100%; height: 100%; z-index: 9999; } .nightnight-skip a {padding: 10px 0; } .nightnight a:link {color: #555; } .nightnight a:visited {color: #555; } .nightnight a:hover {color: #FFF; } .nightnight-timing {font-size: 15px; font-weight: 900; margin-top: 20px; } .nightnight-skip {font-style: italic; font-size: 14px; margin-top: 40px; } .nightnight-credits {position: fixed; bottom: 20px; right: 20px; margin-top: 40px; font-size:9px; }';
-		var head = document.head || document.getElementsByTagName('head')[0];
-		var style = document.createElement('style');
-		style.type = 'text/css';
-		if (style.styleSheet){
-			style.styleSheet.cssText = css;
-		} else {
-			style.appendChild(document.createTextNode(css));
-		}
-		head.appendChild(style);
+      // Inject CSS
+      var css = '.nightnight {background: #000; font-family: Arial, sans-serif; color: #fff; font-size: 12px; padding: 10%; position: fixed; margin: 0; top: 0; left: 0; right: 0; bottom: 0; z-index: 9999; } .nightnight-skip a {padding: 10px 0; } .nightnight a:link {color: #555; } .nightnight a:visited {color: #555; } .nightnight a:hover {color: #FFF; } .nightnight-timing {font-size: 15px; font-weight: 900; margin-top: 20px; } .nightnight-skip {font-style: italic; font-size: 14px; margin-top: 40px; } .nightnight-credits {position: fixed; bottom: 20px; right: 20px; margin-top: 40px; font-size:9px; }';
+      var head = document.head || document.getElementsByTagName('head')[0];
+      var style = document.createElement('style');
+      style.type = 'text/css';
+      if (style.styleSheet){
+        style.styleSheet.cssText = css;
+      } else {
+        style.appendChild(document.createTextNode(css));
+      }
+      head.appendChild(style);
 
-		// Format time
-		var time = ''
-		if (use12HourClock) {
-			var suffix = eh < 12 ? "AM" : "PM"
-			time = (eh % 13) + ":" + ("0"+em).slice(-2) + " " + suffix;
-		} else {
-			time = ("0"+eh).slice(-2) + ":" + ("0"+em).slice(-2)
-		}
+      // Format time
+      var time = ''
+      if (use12HourClock) {
+        var suffix = eh < 12 ? "AM" : "PM"
+        time = (eh % 13) + ":" + ("0"+em).slice(-2) + " " + suffix;
+      } else {
+        time = ("0"+eh).slice(-2) + ":" + ("0"+em).slice(-2)
+      }
 
-		// Inject DIV
-		var wrapper = document.createElement('div');
-		wrapper.innerHTML = '<div class="nightnight"><div>It&rsquo;s late&hellip;</div><div> Nothing we can offer you is more important than your sleep.</div> <div> Sleep well, sweet dreams, and we will catch you in the morning.</div> <div class="nightnight-timing">This site will wake up at ' + 
-			time + 
-			'</div> <div class="nightnight-skip"> <a href="javascript:nightnightHide()">I&rsquo;ll sleep later, I really need to use this site right now &rightarrow;</a> </div> <div class="nightnight-credits"> <a href="https://www.nightnight.xn--q9jyb4c"><img src="https://code.nightnight.xn--q9jyb4c/static/night_night_everyone_white.png" alt="night night" height="32" width="161"></a></div> </div>';
-		document.body.appendChild(wrapper);
+      // Inject DIV
+      var wrapper = document.createElement('div');
+      wrapper.innerHTML = '<div class="nightnight"><div>It&rsquo;s late&hellip;</div><div> Nothing we can offer you is more important than your sleep.</div> <div> Sleep well, sweet dreams, and we will catch you in the morning.</div> <div class="nightnight-timing">This site will wake up at ' + 
+        time + 
+        '</div> <div class="nightnight-skip"> <a href="javascript:nightnightHide()">I&rsquo;ll sleep later, I really need to use this site right now &rightarrow;</a> </div> <div class="nightnight-credits"> <a href="https://www.nightnight.xn--q9jyb4c"><img src="https://code.nightnight.xn--q9jyb4c/static/night_night_everyone_white.png" alt="night night" height="32" width="161"></a></div></div>';
+      document.body.appendChild(wrapper);
+    }
 	}
 })();
